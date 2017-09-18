da5857c1495fb2ff2e66db60a27a3b52d589cdc1
Henrique Nascimento <henrique.pan@hotmail.com>
2017-09-18 00:38:04 -0400

    Studies of collection view and dimensions

--- collection-view-test/ViewController.swift
+++ collection-view-test/ViewController.swift
@@ -9,17 +9,74 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    let reuseIdentifier = "collectionCell"
+    @IBOutlet weak var collectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.collectionView.delegate = self
+        self.collectionView.dataSource = self
+        
+        collectionView.translatesAutoresizingMaskIntoConstraints = true
+        
+        print("Setting initial dimesions")
+        setCollectionViewDimensions()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func didRotate(from fromInterfaceOrientation: UIInterfaceOrientation) {
+        setCollectionViewDimensions()
     }
-
-
+    
+    private func setCollectionViewDimensions() {
+        let orient = UIApplication.shared.statusBarOrientation
+        
+        if (orient.isPortrait) {
+            print("Portrait")
+            let width = self.view.frame.width * 0.85
+            let height = width
+            
+            let x = (self.view.frame.width - width) / 2
+            let y = (self.view.frame.height - width) / 2
+            
+            collectionView.frame = CGRect(x: x, y: y, width: width, height: height)
+        } else {
+            print("LandScape")
+            let width = self.view.frame.width * 0.70
+            let height = width
+            
+            let x = (self.view.frame.width - width) / 2
+            let y = (self.view.frame.height - width) / 2
+            
+            collectionView.frame = CGRect(x: x, y: y, width: width, height: height)
+        }
+        
+        collectionView.reloadData()
+    }
+    
 }
 
+extension ViewController: UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 12
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let itemWidth: CGFloat = collectionView.frame.width / 4
+        let itemHeight: CGFloat = collectionView.frame.height / 4
+        return CGSize(width: itemWidth, height: itemHeight)
+    }
+}
