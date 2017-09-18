8e9fa6362f5e5c051c36c2fdf2d0c0353e50b418
Henrique Nascimento <henrique.pan@hotmail.com>
2017-09-17 21:25:56 -0400

    Initial Commit

---
+++ collection-view-test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  collection-view-test
+//
+//  Created by Henrique Nascimento on 2017-09-17.
+//  Copyright © 2017 Henrique Nascimento. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
